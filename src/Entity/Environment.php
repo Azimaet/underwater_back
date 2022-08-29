ed7095f623bf0f6befffd0146c4c0c7bb4024243
azimaet <maximedolo@gmail.com>
2022-08-29 17:51:44 +0200

    feat. apiplatform, extends class as api resources

--- src/Entity/Environment.php
+++ src/Entity/Environment.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\EnvironmentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EnvironmentRepository::class)]
+#[ApiResource()]
 class Environment
 {
     #[ORM\Id]
